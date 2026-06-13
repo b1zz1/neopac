f3ce60e04f23bddae4347ccf1b24cec3aced4cc5
Eduardo Martins <eduardobizzi2005@gmail.com>
2026-06-13 15:13:09 -0300

    NEO-005: ✨ Versatile map renderer function & level 1

--- pac-man.py
+++ pac-man.py
@@ -1,21 +1,21 @@
 import pygame
 
 import config as cfg
+import levels.level_1 as lvl_1
 from board import draw_board
-from levels.level_1 import board
 
 pygame.init()
 
 timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font(None, 20)  # Improve font choice
-level = board
+level = lvl_1
 
 run = True
 while run:
     timer.tick(fps)
-    cfg.screen.fill("black")
-    draw_board(level)
+    cfg.screen.fill('black')
+    draw_board(level.board, level.color)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
