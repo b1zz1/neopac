021d9db60dbe41351f74d59b32759df55b88d93e
Eduardo Martins <eduardobizzi2005@gmail.com>
2026-06-13 19:15:54 -0300

    ✨ NEO-003: Temporary Pac-man visual and animation

--- game.py
+++ game.py
@@ -3,19 +3,29 @@ import pygame
 import config as cfg
 import levels.level_1 as lvl_1
 from board import draw_board
+from player import draw_player
 
 pygame.init()
 
-timer = pygame.time.Clock()
-fps = 60
+FPS = 60
 font = pygame.font.Font(None, 20)  # Improve font choice
 level = lvl_1
 
+timer = pygame.time.Clock()
+counter = 0
+
 run = True
 while run:
-    timer.tick(fps)
+    timer.tick(FPS)
+    if counter < 19:
+        counter += 1
+    else:
+        counter = 0
+
     cfg.screen.fill('black')
+
     draw_board(level.board, level.color)
+    draw_player(counter)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
